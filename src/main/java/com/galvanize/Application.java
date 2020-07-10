ffe1ea07810e7141d461a9cdb092b98fac7d394f
Jason Taylor <taylorjason@users.noreply.github.com>
2020-07-10 14:26:29 -0400

    initial commit

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,12 @@
+package com.galvanize;
+
+/**
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
